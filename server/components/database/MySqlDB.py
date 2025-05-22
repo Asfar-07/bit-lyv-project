a4070c59b0afe05543eaa362794d1b2bb628bce2
Asfar-07 <asfarbismi@gmail.com>
2025-05-22 23:28:07 +0530

    improve security

--- server/components/database/MySqlDB.py
+++ server/components/database/MySqlDB.py
@@ -1,12 +1,13 @@
 import mysql.connector
+import os
 from dotenv  import dotenv_values,load_dotenv
 load_dotenv()
 config=dotenv_values('.env')
 def DataBase():
     mydb = mysql.connector.connect(
-    host=config["DATA_BASE_HOST"],
-    user=config["DATA_BASE_USERNAME"],
-    password=config['DATA_BASE_PASSWORD'],
-    database=config['DATA_BASE_NAME']
+    host=os.getenv("DATA_BASE_HOST"),
+    user=os.getenv("DATA_BASE_USERNAME"),
+    password=os.getenv('DATA_BASE_PASSWORD'),
+    database=os.getenv('DATA_BASE_NAME')
     )
     return mydb
